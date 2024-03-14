2170547ac7215b0ea530cac349485613d575c500
Vladimir <107743566+BajeProg@users.noreply.github.com>
2024-03-14 15:11:34 +0500

    Add files via upload

--- Project/api/home/addapp.php
+++ Project/api/home/addapp.php
@@ -5,6 +5,6 @@ session_start();
 $currentDateTime = new DateTime();
 
 database_query("INSERT INTO `API_keys`(`Token`, `App_name`, `User_ID`) 
-VALUES ('".hash("sha256", $_POST["appName"].session_id().$currentDateTime->format('Y-m-d H:i:s'))."', '".$_POST["appName"]."', ".$_SESSION["userID"].")")
+VALUES ('".hash("sha256", $_POST["appName"].session_id().$currentDateTime->format('Y-m-d H:i:s'))."', '".$_POST["appName"]."', ".$_SESSION["userID"].")");
 
 header('Location: index.php');
--- Project/api/home/deleteapp.php
+++ Project/api/home/deleteapp.php
@@ -2,5 +2,5 @@
 include_once("../functions.php");
 
 session_start();
-database_query("DELETE FROM `API_keys` WHERE `User_ID` = ".$_SESSION["userID"]." AND `ID` = ".$_GET["id"])
+database_query("DELETE FROM `API_keys` WHERE `User_ID` = ".$_SESSION["userID"]." AND `ID` = ".$_GET["id"]);
 header('Location: index.php');
